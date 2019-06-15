53619f7b37c9de8b12f1a71a0a1b6753da58d4bc
Trần Đức Minh <36166484+tranducminh@users.noreply.github.com>
2019-06-15 18:00:15 +0700

    update view

---
+++ app/models/userModel.js
@@ -0,0 +1,59 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+let bcrypt = require('bcrypt-nodejs');
+
+let userSchema = new Schema({
+    local: {
+        fullname: String,
+        email: String,
+        username: String,
+        password: String,
+        photoURL:{
+            type: String
+        },
+        locale: String
+
+    },
+    facebook: {
+        id: String,
+        token: String,
+        displayname: String,
+        email: String,
+        firstname: String,
+        lastname: String,
+        fullname: String,
+        middlename: String,
+        photoURL: {
+            type: String,
+            //default: "",
+        }
+    },
+    google: {
+        id: String,
+        token: String,
+        displayname: String,
+        firstname: String,
+        lastname: String,
+        fullname: String,
+        photoURL: String,
+        locale: String
+    }
+})
+
+/**
+ * Tạo mã hóa mật khẩu
+ */
+userSchema.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
+
+/**
+ * Kiểm tra mật khẩu có trùng khớp
+ */
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password);
+}
+
+let users = mongoose.model("user", userSchema);     //lưu schema vào database
+
+module.exports = users;
--- public/js/header.js
+++ public/js/header.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded",function(){
-    let menu_icon = document.querySelector('.header__right__menu__icon');
-    let user_icon = document.querySelector('.header__right__menu__user');
-    let menu = document.querySelector('.header__right__menu__option');
-    
-    menu_icon.onclick = function () {
-        menu.classList.toggle('display');
+    let menu_icon_notAuthenticated = document.querySelector('.header__right__menuNotAuthenticated__icon');
+    let user_icon_anthenticated = document.querySelector('.header__right__menuAuthenticated__user');
+    let menu_notAuthenticated = document.querySelector('.header__right__menuNotAuthenticated__option');
+    let menu_authenticated = document.querySelector('.header__right__menuAuthenticated__option');
+
+    menu_icon_notAuthenticated.onclick = function () {
+        menu_notAuthenticated.classList.toggle('display');
     }
-    user_icon.onclick = function () {
-        menu.classList.toggle('display');
+    user_icon_anthenticated.onclick = function () {
+        menu_authenticated.classList.toggle('display');
     }
 },false);
