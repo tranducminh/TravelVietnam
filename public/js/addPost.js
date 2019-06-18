baabfcb51b1a38ef92ab60d698f52cead9e7905a
Trần Đức Minh <36166484+tranducminh@users.noreply.github.com>
2019-06-18 11:01:43 +0700

    update addPost page

---
+++ public/js/addPost.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let schedule = document.querySelector('#schedule table tr');
+    let add_schedule = document.querySelector('#schedule_plus')
+
+    add_schedule.onclick = function () {
+        console.log(schedule);
+        schedule.insertAdjacentHTML("afterend",
+            "<tr>" +
+                "<td class='first'>" +
+                    "<textarea name='' id='' rows='1' class='time font font--bold'" +
+                        "placeholder='Thời gian'></textarea>" +
+                " </td>" +
+                "<td class='second'>" +
+                    "<textarea name='' id='' cols='30' rows='2' class='font font--bold font--space' " +
+                        "placeholder='Tiêu đề'></textarea>" +
+                    "<textarea name='' id='' cols='30' rows='5' class='font' " +
+                        "placeholder='Chi tiết hoạt động'></textarea>" +
+                "</td>" +
+            "</tr>"
+        )
+    }
+
+    let policy = document.querySelector('#policy table tr');
+    let add_policy = document.querySelector('#policy_plus');
+
+    add_policy.onclick = function () {
+        policy.insertAdjacentHTML('afterend',
+            "<tr>"+
+                "<td style='border: none'>"+
+                    "<textarea name='' id='' cols='100' rows='1' class='font'"+
+                        "placeholder='...'></textarea>"+
+                "</td>"+
+            "</tr>"
+        )
+    }
+
+})
